3c85b3d2711782075dacfc80d30b7891f6afdccf
vamsi krishna katam <vk84351@gmail.com>
2019-11-05 03:19:57 -0800

    Update and rename main.py to math.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import calc
-
-print(calc.factorial(5))
-
-print(calc.log10(1000))
-
-print(calc.radian(1))
-
---
+++ math.py
@@ -0,0 +1,6 @@
+from calc import fact,log10,radToDeg,trig
+print(fact(5))
+print(radToDeg(40))
+trig(45)
+
+
